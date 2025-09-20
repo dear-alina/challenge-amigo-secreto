dcaae9cde9c8e06515205138eaf03b817492eab0
dear-alina <haku16012@gmail.com>
2025-09-20 17:10:51 -0500

    Primera funcion creada

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos= [];
 
+function asignarTextoElemento(elemento,texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return
+}
