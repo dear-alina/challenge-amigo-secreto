575898426aa08af0f7923bec39c76a2b68c4b251
dear-alina <haku16012@gmail.com>
2025-09-20 21:31:45 -0500

    Se agregaron funciones

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigoSecreto = 0;
 let amigos= [];
 
+// Función para asiganción de texto
 function asignarTextoElemento(elemento,texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
-    return
+    return;
 }
+
+//Función para validar y almacenar el nombre de los amigos ingresados
+function agregarAmigos(){
+    //Ingreso de nombres
+    let  ingresarNombre= document.getElementById("amigo");
+    //Obtener nmbre ingresado
+    let nombre = ingresarNombre.value;
+   //Funcion que valide que el nombre no esté vacío
+    if(nombre != ""){
+        asignarTextoElemento("h2","Agrega a tus amigos que falten ✨");
+    } else{ 
+            alert("Por favor, ingresa un nombre unu");
+            return;
+        }   
+    amigos.push(nombre); //Ingresa los valores en la lista
+    console.log(amigos);
+    limpiarCaja();
+}
+
+//Esta función limpiará la caja de entrada cada que se añada un nombre
+function limpiarCaja(){
+    document.querySelector("#amigo").value= "";
+}
+
+//Funcion para las condiciones iniciales en el juego
+
+function condicionesIniciales() {
+    asignarTextoElemento("h2","Ingresa el nombre de tus amigos uwu");
+    agregarAmigos();  
+}
+
+condicionesIniciales();
